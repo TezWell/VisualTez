63801da71bedb7b6db2ac5de99e38b6b46336bfa
Rodrigo Quelhas <rodrigo_quelhas@outlook.pt>
2022-03-31 14:16:01 +0100

    Remove 404 svg on not-found page

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -1,26 +1,8 @@
 import React from 'react';
 
 const NotFound = () => (
-    <div className="flex-1 flex justify-center items-center">
-        <svg viewBox="0 0 345 200" width={345} className="block">
-            <text
-                x="50%"
-                y="50%"
-                fontStyle="italic"
-                fontWeight="bold"
-                fontStretch="normal"
-                fontSize="192px"
-                fontFamily="sans-serif"
-                letterSpacing="0px"
-                fill="currentColor"
-                fillOpacity="1"
-                stroke="none"
-                dominantBaseline="middle"
-                textAnchor="middle"
-            >
-                404
-            </text>
-        </svg>
+    <div className="flex-1 inline-flex justify-center items-center">
+        <h1 className="text-[20rem] italic font-black tracking-widest diagonal-fractions">404</h1>
         <p>
             Page <b className="italic tracking-wider">{window.location.pathname}</b> does not exist!
         </p>
