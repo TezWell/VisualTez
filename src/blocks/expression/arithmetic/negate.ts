fe55537d55e56b5f5375c5f1b6e4ceaab5e957dd
Rodrigo Quelhas <rodrigo_quelhas@outlook.pt>
2022-04-07 10:35:50 +0100

    feat: Adds negate expression

---
+++ src/blocks/expression/arithmetic/negate.ts
@@ -0,0 +1,47 @@
+import type { Block } from 'blockly';
+import Blockly from 'blockly';
+
+import { Negate } from '@tezwell/smartts-sdk/expression';
+import { IExpression } from '@tezwell/smartts-sdk/typings/expression';
+
+import BlockKind from '../../enums/BlockKind';
+import SmartML from '../../generators/SmartML';
+import { buildErrorInfo } from '../../utils/errorHandling';
+
+const NegateBlock = {
+    type: BlockKind.negate_expression,
+    message0: 'Negate %1',
+    args0: [
+        {
+            type: 'input_value',
+            name: 'VALUE',
+            check: ['Nat', 'Int', 'Bls12_381_g1', 'Bls12_381_g2', 'Bls12_381_fr', 'Expression'],
+        },
+    ],
+    tooltip: `
+    Negate a numerical value.\n-\n
+        TNat() => TInt()\n
+        TInt() => TInt()\n
+        TBls12_381_g1())=> TBls12_381_g1()\n
+        TBls12_381_g2() => TBls12_381_g2()\n
+        TBls12_381_fr() => TBls12_381_fr()
+    `,
+    inputsInline: true,
+    output: ['Expression'],
+    colour: 200,
+};
+
+Blockly.Blocks[BlockKind.negate_expression] = {
+    init: function () {
+        this.jsonInit(NegateBlock);
+        this.setPreviousStatement(false);
+        this.setNextStatement(false);
+    },
+};
+
+SmartML.addBlock(BlockKind.negate_expression, {
+    toValue: (block: Block) => {
+        const value: IExpression<any> = SmartML.toValue(block, 'VALUE');
+        return Negate(value, buildErrorInfo(block));
+    },
+});
