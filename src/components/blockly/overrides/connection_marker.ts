f01c356d82018c8c7314215f934da0d3c5adb159
Rodrigo Quelhas <rodrigo_quelhas@outlook.pt>
2022-05-08 22:17:36 +0100

    feat(UX/UI): highlights output connections on zelos renderer

--- src/components/blockly/overrides/connection_marker.ts
+++ src/components/blockly/overrides/connection_marker.ts
@@ -1,48 +1,74 @@
 import Blockly from 'blockly';
 
-// @IMPORTANT
+/**
+ * Highlights the input value connection
+ *
+ * @override
+ */
 Blockly.BlockSvg.prototype.highlightShapeForInput = function (this: any, conn: any, add: any) {
-    console.error('T', add, conn, this.width, this);
-
-    if (add) {
-        let colour;
-        let size;
-        let fill;
-        if (conn.type === Blockly.ConnectionType.INPUT_VALUE) {
-            size = 5;
-            colour = 'magenta';
-            fill = 'blue';
-        } else if (conn.type === Blockly.ConnectionType.OUTPUT_VALUE) {
-            size = 2;
-            colour = 'magenta';
-            fill = colour;
-        } else if (conn.type === Blockly.ConnectionType.NEXT_STATEMENT) {
-            size = 4;
-            colour = 'goldenrod';
-            fill = 'none';
-        } else if (conn.type === Blockly.ConnectionType.PREVIOUS_STATEMENT) {
-            size = 2;
-            colour = 'goldenrod';
-            fill = colour;
-        }
-        // Add connection indicator
-        this.svgConnection_ = Blockly.utils.dom.createSvgElement(
-            Blockly.utils.Svg.CIRCLE,
-            {
-                className: 'connectionMark',
-                cx: conn.offsetInBlock_.x,
-                cy: conn.offsetInBlock_.y,
-                r: size,
-                fill: fill,
-                stroke: colour,
-                'stroke-width': 2,
-            },
-            this.svgGroup_,
-        );
-    } else if (this.svgConnection_) {
-        // Remove connection indicator
-        this.svgGroup_.removeChild(this.svgConnection_);
-        this.svgConnection_ = null;
-    }
+    highlightConnection(this, conn, add);
     this.pathObject.updateShapeForInputHighlight(conn, add);
+};
+
+/**
+ * Highlights the output connection when dragging a block
+ *
+ * @override
+ */
+const original_setDragging = Blockly.BlockSvg.prototype.setDragging;
+Blockly.BlockSvg.prototype.setDragging = function (this, adding: boolean) {
+    highlightConnection(this, this.outputConnection, adding);
+    original_setDragging.call(this, adding);
+};
+
+/**
+ * Highlights a connection of a block.
+ *
+ * @param block The block that contains the connection
+ * @param conn The connection to be highlighted
+ * @param add Indicates when to add the highlight effect and when to remove it
+ */
+const highlightConnection = (block: any, conn: any, add: boolean) => {
+    if (conn) {
+        if (add) {
+            let colour;
+            let size;
+            let fill;
+            if (conn.type === Blockly.ConnectionType.INPUT_VALUE) {
+                size = 5;
+                colour = 'magenta';
+                fill = 'blue';
+            } else if (conn.type === Blockly.ConnectionType.OUTPUT_VALUE) {
+                size = 5;
+                colour = 'magenta';
+                fill = 'blue';
+            } else if (conn.type === Blockly.ConnectionType.NEXT_STATEMENT) {
+                size = 4;
+                colour = 'goldenrod';
+                fill = 'none';
+            } else if (conn.type === Blockly.ConnectionType.PREVIOUS_STATEMENT) {
+                size = 2;
+                colour = 'goldenrod';
+                fill = colour;
+            }
+            // Add connection indicator
+            block.blockConnection_ = Blockly.utils.dom.createSvgElement(
+                Blockly.utils.Svg.CIRCLE,
+                {
+                    className: 'connectionMark',
+                    cx: conn.offsetInBlock_.x,
+                    cy: conn.offsetInBlock_.y,
+                    r: size,
+                    fill: fill,
+                    stroke: colour,
+                    'stroke-width': 2,
+                },
+                block.svgGroup_,
+            );
+        } else if (block.blockConnection_) {
+            // Remove connection indicator
+            block.svgGroup_.removeChild(block.blockConnection_);
+            block.blockConnection_ = null;
+        }
+    }
 };
